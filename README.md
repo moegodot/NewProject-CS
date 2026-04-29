e7d122c8181b474521b9fd5242c829a1bd1ed452
godot <me@kawayi.moe>
2026-04-30 06:07:21 +0800

    udpate

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Welcome to Moe Godot's Open Source Project
 
-![打碎旧世界,建设新世界](https://raw.githubusercontent.com/moegodot/NewProject-CS/refs/heads/master/images/readme_image_1.jpg)
+![打碎旧世界,建设新世界](https://raw.githubusercontent.com/moegodot/NewProject-CS/refs/heads/master/images/readme_image_1.png)
 
 ## Brief And Features
 
--- engineering/Project.Maintainer/Maintenance.cs
+++ engineering/Project.Maintainer/Maintenance.cs
@@ -9,14 +9,31 @@ public abstract class Maintenance
 {
     public abstract string Paths();
 
+    public const string StartColor = "\e[32;45m";
+    public const string StopColor = "\e[0m";
+
     public void Maintain()
     {
         var paths = Paths();
         var src = Path.Combine(Program.UpdateContentDirectory, paths);
         var dst = Path.Combine(Program.RepoDirectory, paths);
         Helper.ShallowCheckout(Program.UpdateContentDirectory, paths);
+
+        if (File.Exists(src))
+        {
+            File.Delete(dst);
+            File.Copy(src,dst);
+            Console.WriteLine($"{StartColor}copy file from {src} to {dst}{StopColor}");
+        }
+
+        if (!Directory.Exists(src))
+        {
+            return;
+        }
+
         Directory.Delete(dst);
         Directory.Move(src,
                        dst);
+        Console.WriteLine($"{StartColor}copy directory from {src} to {dst}{StopColor}");
     }
 }
